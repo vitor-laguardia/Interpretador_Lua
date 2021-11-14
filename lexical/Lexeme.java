ac12904a2f0c8367428e0bedba69d7eff284701c
Vitor Laguardia Xavier <vitor.laguardia@hotmail.com>
2021-11-14 14:20:21 -0300

    add Lexeme class

---
+++ lexical/Lexeme.java
@@ -0,0 +1,15 @@
+package lexical;
+
+public class Lexeme {
+
+	// public variables for easy access.
+	public String token;
+	public TokenType type;
+
+	// Lexeme constructor 
+	public Lexeme(String token, TokenType type) {
+		this.token = token;
+		this.type = type;
+	}
+
+}
