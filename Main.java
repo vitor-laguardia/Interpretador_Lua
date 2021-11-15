411b7dfdfdfdbe9b38a85740f468de7ac5cc0bd9
Vitor Laguardia Xavier <vitor.laguardia@hotmail.com>
2021-11-15 15:15:56 -0300

    added main file

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import lexical.LexicalAnalysis;
+import lexical.Lexeme;
 
 public class Main {
     public static void main(String args[]) {
         try {
-            LexicalAnalysis obj = new LexicalAnalysis("./teste");
-            System.out.println(obj);
+            LexicalAnalysis obj = new LexicalAnalysis("./teste.lua");
+            System.out.println(obj.nextToken());
         } catch (Exception e) {
             //TODO: handle exception
         }
