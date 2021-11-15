93d89c3b1789e2cd5cbcc516749179c298f061af
Victor Luiz <c0007043@vli-logistica.com.br>
2021-11-15 13:42:35 -0300

    indentation comments in TokenType

--- lexical/TokenType.java
+++ lexical/TokenType.java
@@ -2,62 +2,61 @@ package lexical;
 
 public enum TokenType {
 	// Keywords
-	IF,						// if
-	WHILE,		  	// while
-	REPEAT,     	// repeat
-	FOR,        	// for
-	PRINT,      	// print
-	THEN,       	// then
-	ELSE_IF,    	// else if
-	ELSE,      	  // else
-	UNTIL,     	 	// until
-	IN,        		// in
-	DO,        		// do
-	END,       		// end
+	IF,									// if
+	WHILE,		  				// while
+	REPEAT,     				// repeat
+	FOR,        				// for
+	PRINT,      				// print
+	THEN,       				// then
+	ELSE_IF,    				// else if
+	ELSE,      	  			// else
+	UNTIL,     	 				// until
+	IN,        					// in
+	DO,        					// do
+	END,       					// end
 
 	// Logic operators
-	EQUAL,         // ==
-	NOT_EQUAL,     // ~=
-	LOWER,         // <
-	LOWER_EQUAL,   // <=
-	GREATER,       // >
-	GREATER_EQUAL, // >=
-	AND,           // and
-	OR,            // or
-	TRUE,          // true
-	FALSE,         // false
+	EQUAL,         			// ==
+	NOT_EQUAL,     			// ~=
+	LOWER,         			// <
+	LOWER_EQUAL,   			// <=
+	GREATER,       			// >
+	GREATER_EQUAL, 			// >=
+	AND,           			// and
+	OR,            			// or
+	TRUE,          			// true
+	FALSE,         			// false
 
 	// Arithmetic operators
-	ADD,           // +
-	SUB,           // -
-	MUL,           // *
-	DIV,           // /
-	MOD,					 // %
+	ADD,          	 		// +
+	SUB,           			// -
+	MUL,           			// *
+	DIV,           			// /
+	MOD,					 			// %
 
 	// Symbols
-	SEMICOLON,         // ;
-	DOT,               // .
-	COMMA,             // ,
-	OPEN_PARENTHESES,	 // (
-	CLOSE_PARENTHESES, // )
-	OPEN_BRACKET,      // [
-	CLOSE_BRACKET,     // ]
-	OPEN_KEYS,         // {
-	CLOSE_KEYS,        // }
-	ASSIGN,            // =
-	HASHTAG,           // #
-	QUOTATION_MARKS,   // "" || ''
-	COMMENTARY,       // --
-	CONCAT,				// ..
+	SEMICOLON,         	// ;
+	DOT,               	// .
+	COMMA,             	// ,
+	OPEN_PARENTHESES,	 	// (
+	CLOSE_PARENTHESES, 	// )
+	OPEN_BRACKET,      	// [
+	CLOSE_BRACKET,     	// ]
+	OPEN_KEYS,         	// {
+	CLOSE_KEYS,        	// }
+	ASSIGN,            	// =
+	HASHTAG,           	// #
+	QUOTATION_MARKS,   	// "" || ''
+	COMMENTARY,         // --
+	CONCAT,							// ..
 	
-
 	// Functions
-	READ,             // read
-	TO_STRING,	      // converter for string (number, double ..)
-	TO_NUMBER,        // converter string for number
+	READ,             	// read
+	TO_STRING,	      	// converter for string (number, double ..)
+	TO_NUMBER,        	// converter string for number
 
 	// Others 
-	NUMBER,           // type numeric
-	VAR,              // string
-	NIL,              // null
+	NUMBER,           	// type numeric
+	VAR,              	// string
+	NIL,              	// null
 }
