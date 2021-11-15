52e49dc692e54d930bbda986a56d30e84193809d
Victor Luiz <54218185+victorluizskt@users.noreply.github.com>
2021-11-15 14:04:13 -0300

    Update commentary

--- lexical/TokenType.java
+++ lexical/TokenType.java
@@ -8,18 +8,18 @@ public enum TokenType {
 	END_OF_FILE,
 
 	// Keywords
-	IF,									// if
-	WHILE,		  				// while
-	REPEAT,     				// repeat
-	FOR,        				// for
-	PRINT,      				// print
-	THEN,       				// then
-	ELSE_IF,    				// else if
-	ELSE,      	  			// else
-	UNTIL,     	 				// until
-	IN,        					// in
-	DO,        					// do
-	END,       					// end
+	IF,				// if
+	WHILE,		  		// while
+	REPEAT,     			// repeat
+	FOR,        			// for
+	PRINT,      			// print
+	THEN,       			// then
+	ELSE_IF,    			// else if
+	ELSE,      	  		// else
+	UNTIL,     	 		// until
+	IN,        			// in
+	DO,        			// do
+	END,       			// end
 
 	// Logic operators
 	EQUAL,         			// ==
@@ -38,31 +38,31 @@ public enum TokenType {
 	SUB,           			// -
 	MUL,           			// *
 	DIV,           			// /
-	MOD,					 			// %
+	MOD,				// %
 
 	// Symbols
-	SEMICOLON,         	// ;
-	DOT,               	// .
-	COMMA,             	// ,
+	SEMICOLON,         		// ;
+	DOT,               		// .
+	COMMA,             		// ,
 	OPEN_PARENTHESES,	 	// (
-	CLOSE_PARENTHESES, 	// )
-	OPEN_BRACKET,      	// [
-	CLOSE_BRACKET,     	// ]
-	OPEN_KEYS,         	// {
-	CLOSE_KEYS,        	// }
-	ASSIGN,            	// =
-	HASHTAG,           	// #
-	QUOTATION_MARKS,   	// "" || ''
-	COMMENTARY,         // --
-	CONCAT,							// ..
+	CLOSE_PARENTHESES, 		// )
+	OPEN_BRACKET,      		// [
+	CLOSE_BRACKET,     		// ]
+	OPEN_KEYS,         		// {
+	CLOSE_KEYS,        		// }
+	ASSIGN,            		// =
+	HASHTAG,           		// #
+	QUOTATION_MARKS,   		// "" || ''
+	COMMENTARY,         		// --
+	CONCAT,				// ..
 	
 	// Functions
-	READ,             	// read
-	TO_STRING,	      	// converter for string (number, double ..)
-	TO_NUMBER,        	// converter string for number
+	READ,             		// read
+	TO_STRING,	      		// converter for string (number, double ..)
+	TO_NUMBER,        		// converter string for number
 
 	// Others 
-	NUMBER,           	// type numeric
-	VAR,              	// string
-	NIL,              	// null
+	NUMBER,           		// type numeric
+	VAR,              		// string
+	NIL,              		// null
 }
