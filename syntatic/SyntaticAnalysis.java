c0c6be8956b6602b9d71611b64912f5d8a8a4280
Victor Luiz <c0007043@vli-logistica.com.br>
2021-11-24 20:39:33 -0300

    implemented syntaticAnalysis

---
+++ syntatic/SyntaticAnalysis.java
@@ -0,0 +1,84 @@
+package syntatic;
+import lexical.Lexeme;
+import lexical.LexicalAnalysis;
+import lexical.TokenType;
+
+public class SyntaticAnalysis {
+
+  private LexicalAnalysis lexical;
+  private Lexeme current;
+
+  public SyntaticAnalysis(LexicalAnalysis lex) {
+    this.lexical = lex;
+    this.current = lex.nextToken();
+  }
+
+  private void advance() {
+    current = lexical.nextToken();
+  }
+
+  private void eat(TokenType type) {
+    if (type == current.type) {
+      advance();
+    } else {
+      showError();
+    }
+  }
+
+  private void showError() {
+    System.out.printf("%02d: ", lexical.getLine());
+
+    switch (current.type) {
+        case INVALID_TOKEN:
+            System.out.printf("Lexema inválido [%s]\n", current.token);
+            break;
+        case UNEXPECTED_EOF:
+        case END_OF_FILE:
+            System.out.printf("Fim de arquivo inesperado\n");
+            break;
+        default:
+            System.out.printf("Lexema não esperado [%s]\n", current.token);
+            break;
+    }
+    System.exit(1);
+  }
+
+  // <program>   ::= program <cmdlist>
+  private void procProgram() { }
+
+  // <cmdlist>   ::= <cmd> { <cmd> }
+  private void procCmdList() { }
+
+  // <cmd>       ::= (<assign> | <output> | <if> | <while>) ;
+  private void procCmd() { }
+
+  // <assign>    ::= <var> = <intexpr>
+  private void procAssign() { }
+
+  // <output>    ::= output <intexpr>
+  private void procOutput() { }
+
+  // <if>        ::= if <boolexpr> then <cmdlist> [ else <cmdlist> ] done
+  private void procIf() { }
+
+  // <while>     ::= while <boolexpr> do <cmdlist> done
+  private void procWhile() { }
+
+  // <boolexpr>  ::= false | true |
+  //                 not <boolexpr> |
+  //                 <intterm> (== | != | < | > | <= | >=) <intterm>
+  private void procBoolExpr() { }
+
+  // <intexpr>   ::= [ + | - ] <intterm> [ (+ | - | * | / | %) <intterm> ]
+  private void procIntExpr() { }
+
+  // <intterm>   ::= <var> | <const> | read
+  private void procIntTerm() { }
+
+  // <var>       ::= id
+  private void procVar() { }
+
+  // <const>     ::= number
+  private void procConst() { }
+
+}
