5016648622c0b3d9958c53c77ad84eb6c8728277
Victor Luiz <c0007043@vli-logistica.com.br>
2021-12-09 08:30:34 -0300

    add comentaries

--- Main.java
+++ Main.java
@@ -6,11 +6,13 @@ public class Main {
     public static void main(String args[]) {
         try {
             LexicalAnalysis obj = new LexicalAnalysis("./file.lua");
-            // while(obj.nextToken().type != TokenType.END_OF_FILE) {
-            //     obj.nextToken();
-            // }
-            SyntaticAnalysis obj2 = new SyntaticAnalysis(obj);
-            obj2.start();
+            // Rodar a parte Sintatica
+            while(obj.nextToken().type != TokenType.END_OF_FILE) {
+                obj.nextToken();
+            }
+            // Rodar a parte Lexica
+            // SyntaticAnalysis obj2 = new SyntaticAnalysis(obj);
+            // obj2.start();
 
         } catch (Exception e) {
             //TODO: handle exception
