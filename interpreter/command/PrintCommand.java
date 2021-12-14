3cfc5e69d3c604e2d567a70486fd9c7a244e3c00
Vitor Laguardia Xavier <vitor.laguardia@hotmail.com>
2021-12-14 00:22:04 -0300

    created PrintCommand class and the folder command

---
+++ interpreter/command/PrintCommand.java
@@ -0,0 +1,17 @@
+package interpreter.command;
+
+public class PrintCommand extends Command {
+
+    private Expr expr;
+
+    public PrintCommand (int line, Expr expr) {
+        super(line);
+        this.expr = expr;
+    }
+
+    @Override
+    public void execute () {
+        System.out.println("cheguei aq menorzada");
+    }
+
+}
